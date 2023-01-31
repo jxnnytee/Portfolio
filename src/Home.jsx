e0dccf19db6e3c4146f17aaa55248c1f89b81f71
jxnnytee <johnakinyemi94@gmail.com>
2023-01-31 13:02:30 +0000

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,10 @@ import {
   Image,
   Stack,
   Text,
-  useBreakpointValue
+  useBreakpointValue,
+  Link, IconButton, useColorModeValue,
 } from '@chakra-ui/react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 export function Home() {
   return (
@@ -30,9 +32,42 @@ export function Home() {
           </Heading>
           <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
           A Frontend Web Developer with broad knowledge in HTML, CSS,
-          Javascript & React 
+          Javascript & React.
           </Text>
+          <Flex>
+          <Link href="https://github.com/jxnnytee/" target='_blank' 
+          pr={'5px'}>
+            <IconButton
+              aria-label="github"
+              variant="ghost"
+              size="lg"
+              bgGradient='linear(to-l, #0ACDFF, #6DC0D5)'
+              fontSize="3xl"
+              icon={<BsGithub />}
+              _hover={{
+                bg: 'blue.500',
+                color: useColorModeValue('white', 'gray.700'),
+              }}
+              isRound
+            />
+          </Link>
+          <Link href="https://www.linkedin.com/in/john-akinyemi-531a9a263/" target='_blank'>
+                  <IconButton
+                    aria-label="linkedin"
+                    variant="ghost"
+                    size="lg"
+                    bgGradient='linear(to-l, #0ACDFF, #6DC0D5)'
+                    icon={<BsLinkedin size="28px" />}
+                    _hover={{
+                      bg: 'blue.500',
+                      color: useColorModeValue('white', 'gray.700'),
+                    }}
+                    isRound
+                  />
+                </Link>
+                </Flex>
           <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+          
           </Stack>
         </Stack>
       </Flex>
